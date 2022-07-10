3b89f2745730311b5a7063aab4880d1200b23a28
Quentin Ikeno <ikenoq@gmail.com>
2022-07-09 17:26:26 -1000

    Render a list of dogs

--- dog-finder/src/Doglist.js
+++ dog-finder/src/Doglist.js
@@ -4,6 +4,13 @@ const Doglist = ({ dogs }) => {
 	return (
 		<div className="Doglist">
 			<h1>React Dog Finder</h1>
+			{dogs.map((dog) => (
+				<div>
+					<h3>{dog.name}</h3>
+					<img src={`./images/${dog.src}`} alt={dog.name} />
+					<Link exact to={`/dogs/${dog.src}`} />
+				</div>
+			))}
 		</div>
 	);
 };
