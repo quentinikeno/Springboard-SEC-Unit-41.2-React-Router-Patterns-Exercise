4e2f0f5e0926ee4b4956affff4fe2397386c407f
Quentin Ikeno <ikenoq@gmail.com>
2022-07-09 21:56:34 -1000

    Add styles for DogLink

--- dog-finder/src/DogLink.js
+++ dog-finder/src/DogLink.js
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import "./DogLink.css";
 
 const DogLink = ({ name, src }) => {
 	return (
-		<div>
+		<div className="DogLink">
 			<Link to={`/dogs/${name.toLowerCase()}`}>
 				<h3>{name}</h3>
 				<img src={src} alt={name} />
