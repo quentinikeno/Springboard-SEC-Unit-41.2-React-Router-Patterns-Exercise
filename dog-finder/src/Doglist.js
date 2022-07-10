8eefa97a0ecb6289debecb8755e9fcb100549dfa
Quentin Ikeno <ikenoq@gmail.com>
2022-07-09 21:48:18 -1000

    Add styles for Doglist

--- dog-finder/src/Doglist.js
+++ dog-finder/src/Doglist.js
@@ -1,12 +1,14 @@
 import DogLink from "./DogLink";
+import "./Doglist.css";
 
 const Doglist = ({ dogs }) => {
+	const allDogs = dogs.map((dog) => (
+		<DogLink name={dog.name} src={dog.src} key={dog.name} />
+	));
 	return (
 		<div className="Doglist">
 			<h1>React Dog Finder</h1>
-			{dogs.map((dog) => (
-				<DogLink name={dog.name} src={dog.src} key={dog.name} />
-			))}
+			<div className="Doglist-list">{allDogs}</div>
 		</div>
 	);
 };
