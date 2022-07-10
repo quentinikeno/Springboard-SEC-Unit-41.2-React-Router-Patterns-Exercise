97cef32ac0de1a46f96939881f233ff9b4932a78
Quentin Ikeno <ikenoq@gmail.com>
2022-07-10 11:43:07 -1000

    Move the list of default prop dogs into its own file

--- dog-finder/src/App.js
+++ dog-finder/src/App.js
@@ -1,11 +1,11 @@
 import { Switch, Route, Redirect } from "react-router-dom";
+
 import Doglist from "./Doglist";
 import DogDetail from "./DogDetail";
 import Navbar from "./Navbar";
-import whiskey from "./imgs/whiskey.jpg";
-import tubby from "./imgs/tubby.jpg";
-import duke from "./imgs/duke.jpg";
-import perry from "./imgs/perry.jpg";
+
+import dogs from "./dogs";
+
 import "./App.css";
 
 function App({ dogs }) {
@@ -27,48 +27,7 @@ function App({ dogs }) {
 }
 
 App.defaultProps = {
-	dogs: [
-		{
-			name: "Whiskey",
-			age: 5,
-			src: whiskey,
-			facts: [
-				"Whiskey loves eating popcorn.",
-				"Whiskey is a terrible guard dog.",
-				"Whiskey wants to cuddle with you!",
-			],
-		},
-		{
-			name: "Duke",
-			age: 3,
-			src: duke,
-			facts: [
-				"Duke believes that ball is life.",
-				"Duke likes snow.",
-				"Duke enjoys pawing other dogs.",
-			],
-		},
-		{
-			name: "Perry",
-			age: 4,
-			src: perry,
-			facts: [
-				"Perry loves all humans.",
-				"Perry demolishes all snacks.",
-				"Perry hates the rain.",
-			],
-		},
-		{
-			name: "Tubby",
-			age: 4,
-			src: tubby,
-			facts: [
-				"Tubby is really stupid.",
-				"Tubby does not like walks.",
-				"Angelina used to hate Tubby, but claims not to anymore.",
-			],
-		},
-	],
+	dogs,
 };
 
 export default App;
