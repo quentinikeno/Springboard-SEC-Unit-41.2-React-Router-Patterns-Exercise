4cdd7895ab1fe984ff3bb1fdd343f2fb87b131fe
Quentin Ikeno <ikenoq@gmail.com>
2022-07-09 21:11:05 -1000

    Compare lowercased names in dogs.find

--- dog-finder/src/DogDetail.js
+++ dog-finder/src/DogDetail.js
@@ -2,13 +2,16 @@ import { useParams } from "react-router-dom";
 
 const DogDetail = ({ dogs }) => {
 	const { name } = useParams();
-	const foundDog = dogs.find((dog) => dog.name === name);
-	const { age, src, facts } = foundDog;
+	const foundDog = dogs.find(
+		(dog) => dog.name.toLowerCase() === name.toLowerCase()
+	);
+	const { name: dogName, age, src, facts } = foundDog;
 	return (
 		<div>
-			<h1>{name}</h1>
-			<h4>{age}</h4>
+			<h1>{dogName}</h1>
+
 			<img src={src} alt={name} />
+			<h4>Age: {age}</h4>
 			<ul>
 				{facts.map((fact) => (
 					<li>{fact}</li>
