739125c3387eee190a087a3e83783ac2c671b183
Quentin Ikeno <ikenoq@gmail.com>
2022-07-09 17:20:58 -1000

    Create dog finder component

---
+++ dog-finder/src/Doglist.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Doglist = ({ dogs }) => {
+	return (
+		<div className="Doglist">
+			<h1>React Dog Finder</h1>
+		</div>
+	);
+};
+
+export default Doglist;
